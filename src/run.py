a59283076e209ee2add22347174bb425df87d056
duarteleonardosales <falecomleonardosales@gmail.com>
2023-03-22 09:10:40 -0300

    first

--- src/run.py
+++ src/run.py
@@ -1,10 +1,9 @@
 import numpy as np
 import pandas as pd
+from dvclive import Live
 from sklearn.metrics import f1_score, r2_score, roc_auc_score
 from sklearn.model_selection import train_test_split
 from xgboost import XGBRegressor
-
-from dvclive import Live
 
 data = pd.read_csv('data/housing.csv')
 data = data.values
@@ -25,5 +24,5 @@ r2 = r2_score(y_test, y_pred)
 corr_pearson = np.corrcoef(y_test, y_pred)
 
 with Live(save_dvc_exp=True) as live:
-    live.log_metric("r2_score", r2)
-    live.log_metric("r", corr_pearson[0][1])
+    live.log_metric("val/r2_score", r2)
+    live.log_metric("val/r", corr_pearson[0][1])
